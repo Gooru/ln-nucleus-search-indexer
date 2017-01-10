530bff30660b880f7c76a590fde42914fe25db3e
Renuka <100067@bq-067.bambeeq.com>
2017-01-10 18:31:42 +0530

    Kafka upgraded to 0.10.0.0

--- src/main/java/org/gooru/nucleus/search/indexers/app/components/KafkaRegistry.java
+++ src/main/java/org/gooru/nucleus/search/indexers/app/components/KafkaRegistry.java
@@ -1,30 +1,31 @@
 package org.gooru.nucleus.search.indexers.app.components;
 
+import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.gooru.nucleus.search.indexers.app.constants.KafkaProperties;
 import org.gooru.nucleus.search.indexers.bootstrap.shutdown.Finalizer;
 import org.gooru.nucleus.search.indexers.bootstrap.startup.Initializer;
-import org.gooru.nucleus.search.indexers.kafka.consumer.KafkaNucleusConsumer;
-import org.gooru.nucleus.search.indexers.kafka.consumer.KafkaKeywordConsumer;
+import org.gooru.nucleus.search.indexers.kafka.consumer.KafkaMessageConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import kafka.consumer.Consumer;
-import kafka.consumer.ConsumerConfig;
-import kafka.javaapi.consumer.ConsumerConnector;
 
 public final class KafkaRegistry implements Initializer, Finalizer {
 
   private static final String DEFAULT_KAFKA_SETTINGS = "defaultKafkaSettings";
   private static final Logger LOGGER = LoggerFactory.getLogger(KafkaRegistry.class);
-  private KafkaNucleusConsumer kafkaNucleusConsumer;
-  private KafkaKeywordConsumer kafkaKeywordConsumer;
+  private KafkaConsumer<String, String> consumer = null;
 
-  private String KAFKA_NUCLEUS_TOPIC = null;
-  private String KAFKA_KEYWORD_TOPIC = null;
+  private String[] KAFKA_TOPICS = null;
+  private static ExecutorService service = Executors.newFixedThreadPool(10);
+
 
   private volatile boolean initialized = false;
 
@@ -47,8 +48,7 @@ public final class KafkaRegistry implements Initializer, Finalizer {
           try {
             JsonObject kafkaConfig = config.getJsonObject(DEFAULT_KAFKA_SETTINGS);
             Properties properties = getProperties(kafkaConfig);
-            initializeNucleusConsumer(properties);
-            initializeKeywordConsumer(properties);
+            createConsumer(properties);
             initialized = true;
             LOGGER.debug("Initializing KafkaRegistry DONE");
           } catch (Exception e) {
@@ -62,61 +62,36 @@ public final class KafkaRegistry implements Initializer, Finalizer {
   private Properties getProperties(JsonObject kafkaConfig) {
     LOGGER.debug("Setting Kafka Properties..");
     final Properties properties = new Properties();
-    properties.put(KafkaProperties.ZK_CONSUMER_CONNECT, kafkaConfig.getString(KafkaProperties.ZK_CONSUMER_CONNECT));
+    properties.put(KafkaProperties.KAFKA_SERVERS, kafkaConfig.getString(KafkaProperties.KAFKA_SERVERS));
     properties.put(KafkaProperties.ZK_CONSUMER_GROUP, kafkaConfig.getString(KafkaProperties.ZK_CONSUMER_GROUP));
     properties.put(KafkaProperties.ZK_SESSION_TIME_OUT_MS, kafkaConfig.getString(KafkaProperties.ZK_SESSION_TIME_OUT_MS));
-    properties.put(KafkaProperties.ZK_SYNCTIME_MS, kafkaConfig.getString(KafkaProperties.ZK_SYNCTIME_MS));
+    properties.put(KafkaProperties.ENABLE_AUTO_COMMIT, kafkaConfig.getBoolean(KafkaProperties.ENABLE_AUTO_COMMIT));
     properties.put(KafkaProperties.AUTOCOMMIT_INTERVAL_MS, kafkaConfig.getString(KafkaProperties.AUTOCOMMIT_INTERVAL_MS));
-    properties.put(KafkaProperties.FETCH_SIZE, kafkaConfig.getString(KafkaProperties.FETCH_SIZE));
     properties.put(KafkaProperties.AUTO_OFFSET_RESET, kafkaConfig.getString(KafkaProperties.AUTO_OFFSET_RESET));
 
-    this.KAFKA_NUCLEUS_TOPIC = kafkaConfig.getString(KafkaProperties.INDEX_NUCLEUS_TOPIC);
-    this.KAFKA_KEYWORD_TOPIC = kafkaConfig.getString(KafkaProperties.INDEX_KEYWORD_TOPIC);
+    properties.put(KafkaProperties.KAFKA_KEY_DESERIALIZER, StringDeserializer.class.getName());
+    properties.put(KafkaProperties.KAFKA_VALUE_DESERIALIZER, StringDeserializer.class.getName());
+
+    this.KAFKA_TOPICS = kafkaConfig.getString(KafkaProperties.KAFKA_TOPICS).split(",");
     return properties;
   }
 
-  private void initializeNucleusConsumer(final Properties properties) {
-    LOGGER.debug("initializeDefaultConsumer now...");
-    kafkaNucleusConsumer = createNucleusConsumer(properties);
-    kafkaNucleusConsumer.start(getKafkaNucleusTopic());
-  }
-
-  private void initializeKeywordConsumer(final Properties properties) {
-    LOGGER.debug("initializeKeywordConsumer now...");
-    kafkaKeywordConsumer = createKeywordConsumer(properties);
-    kafkaKeywordConsumer.start(getKafkaKeywordTopic());
-  }
-
-  public static KafkaNucleusConsumer createNucleusConsumer(final Properties properties) {
-    final ConsumerConfig config = new ConsumerConfig(properties);
-    final ConsumerConnector connector = Consumer.createJavaConsumerConnector(config);
-    return new KafkaNucleusConsumer(connector);
-  }
-  
-  public static KafkaKeywordConsumer createKeywordConsumer(final Properties properties) {
-    final ConsumerConfig config = new ConsumerConfig(properties);
-    final ConsumerConnector connector = Consumer.createJavaConsumerConnector(config);
-    return new KafkaKeywordConsumer(connector);
+  public void createConsumer(final Properties properties) {
+    consumer = new KafkaConsumer<>(properties);
+    consumer.subscribe(Arrays.asList(KAFKA_TOPICS));
+    service.submit(new KafkaMessageConsumer(consumer));
   }
 
   @Override
   public void finalizeComponent() {
-    if (this.kafkaNucleusConsumer != null) {
-      KafkaNucleusConsumer.shutdownMessageConsumer();;
-      this.kafkaNucleusConsumer = null;
+    if (this.consumer != null) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        LOGGER.debug("Kafka Log Consumer unable to wait for 1000ms before it's shutdown");
+      }
+      consumer.close();
     }
-    if (this.kafkaKeywordConsumer != null) {
-      KafkaKeywordConsumer.shutdownMessageConsumer();;
-      this.kafkaKeywordConsumer = null;
-    }
-  }
-
-  public String getKafkaNucleusTopic() {
-    return this.KAFKA_NUCLEUS_TOPIC;
-  }
-  
-  public String getKafkaKeywordTopic() {
-    return this.KAFKA_KEYWORD_TOPIC;
   }
 
   private static final class Holder {
--- src/main/java/org/gooru/nucleus/search/indexers/app/constants/KafkaProperties.java
+++ src/main/java/org/gooru/nucleus/search/indexers/app/constants/KafkaProperties.java
@@ -2,15 +2,17 @@ package org.gooru.nucleus.search.indexers.app.constants;
 
 public final class KafkaProperties {
 
-  public static final String ZK_CONSUMER_CONNECT = "zookeeper.connect";
+  //Kafka Config Constants
   public static final String ZK_CONSUMER_GROUP = "group.id";
   public static final String ZK_SESSION_TIME_OUT_MS = "zookeeper.session.timeout.ms";
-  public static final String ZK_SYNCTIME_MS = "zookeeper.sync.time.ms";
   public static final String AUTOCOMMIT_INTERVAL_MS = "auto.commit.interval.ms";
-  public static final String FETCH_SIZE = "fetch.size";
   public static final String AUTO_OFFSET_RESET = "auto.offset.reset";
-  public static final String INDEX_NUCLEUS_TOPIC = "index.nucleus.topic";
-  public static final String INDEX_KEYWORD_TOPIC = "index.keyword.topic";
+  public static final String KAFKA_SERVERS = "bootstrap.servers";
+  public static final String KAFKA_TOPICS = "consumer.topics";
+  public static final String KAFKA_KEY_DESERIALIZER = "key.deserializer";
+  public static final String KAFKA_VALUE_DESERIALIZER = "value.deserializer";
+  public static final String THREAD_POOL_SIZE ="thead.pool.size";
+  public static final String ENABLE_AUTO_COMMIT = "enable.auto.commit";
 
   private KafkaProperties() {
     throw new AssertionError();
