6b37717ac1d34b2dc44c596bb059a19c89213c64
Renu <100067@bq-067.bambeeq.com>
2018-01-08 18:46:45 +0530

    Support for event based rubric indexing

--- src/main/java/org/gooru/nucleus/search/indexers/app/services/RubricIndexService.java
+++ src/main/java/org/gooru/nucleus/search/indexers/app/services/RubricIndexService.java
@@ -1,11 +1,17 @@
 package org.gooru.nucleus.search.indexers.app.services;
 
+import io.vertx.core.json.JsonObject;
+
 public interface RubricIndexService {
 
   static RubricIndexService instance(){
     return new RubricIndexServiceImpl();
   }
   
+  void indexDocument(String id, JsonObject data) throws Exception;
+
+  void deleteDocument(String id) throws Exception;
+
   void deleteIndexedRubric(String key, String type) throws Exception;
 
 }
